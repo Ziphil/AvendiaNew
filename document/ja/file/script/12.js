ece7464ba23f735b55f331b8bffa5e4c860b0f42
Ziphil <yfe23457@nifty.com>
2019-07-31 19:31:39 +0900

    更新

--- document/ja/file/script/12.js
+++ document/ja/file/script/12.js
@@ -5,7 +5,7 @@ const ALTERNATIVE_URL = "other/other/13.html";
 
 function redirect() {
   let agent = navigator.userAgent;
-  if ((agent.indexOf("iPhone") >= 0 && agent.indexOf("iPad") == -1) || agent.indexOf("iPod") >= 0 || agent.indexOf("Android") >= 0) {
+  if (agent.indexOf("iPhone") >= 0 || agent.indexOf("iPod") >= 0 || agent.indexOf("Android") >= 0) {
     location.href = ALTERNATIVE_URL;
   }
 }
