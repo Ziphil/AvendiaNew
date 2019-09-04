863ad8a2c1e5ee8180d043cd253ef7ccbd349c93
Ziphil <yfe23457@nifty.com>
2019-09-04 23:54:52 +0900

    トップページのスクリプトをTypeScriptに変更

---
+++ document/ja/file/script/12.ts
@@ -0,0 +1,36 @@
+//
+
+
+const DICTIONARY_URL = "conlang/database/1.cgi"
+
+type DayType = {current: 0, difference: number};
+
+export function prepare(): void {
+  fetch("#whole-count", "current", 0);
+  fetch("#week-count", "difference", 7);
+  fetch("#month-count", "difference", 30);
+}
+
+export function fetch<M extends keyof DayType>(query: string, mode: M, day: DayType[M]): void {
+  let request = new XMLHttpRequest();
+  let url = DICTIONARY_URL + "?mode=fetch";
+  if (mode == "current") {
+    url += "&type=1";
+  } else {
+    url += "&type=3";
+    url += "&agree=" + day;
+  }
+  request.open("GET", url, true);
+  request.send(null);
+  request.addEventListener("readystatechange", (event) => {
+    if (request.readyState == 4 && request.status == 200) {
+      let result = request.responseText;
+      let element = document.querySelector(query);
+      if (element != null) {
+        element.textContent = result;
+      }
+    }
+  });
+}
+
+window.onload = prepare;
