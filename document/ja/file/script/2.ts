ff1de3df20f24de6c1c258829a0a86ce6b5f0781
Ziphil <yfe23457@nifty.com>
2019-09-05 10:28:06 +0900

    JavaScriptをTypeScriptに変更

---
+++ document/ja/file/script/2.ts
@@ -0,0 +1,34 @@
+//
+
+
+export function prepare(): void {
+  let element = document.querySelector<HTMLInputElement>("[name=\"search\"]")!;
+  element.addEventListener("keyup", (event) => {
+    let text = element.value;
+    let nextText = convert(text);
+    if (text != nextText) {
+      element.value = nextText;
+    }
+  });
+  element.focus();
+}
+
+export function convert(text: string): string {
+  let nextText = text;
+  nextText = nextText.replace(/aa/g, "â");
+  nextText = nextText.replace(/ee/g, "ê");
+  nextText = nextText.replace(/ii/g, "î");
+  nextText = nextText.replace(/oo/g, "ô");
+  nextText = nextText.replace(/uu/g, "û");
+  nextText = nextText.replace(/ai/g, "á");
+  nextText = nextText.replace(/ei/g, "é");
+  nextText = nextText.replace(/ie/g, "í");
+  nextText = nextText.replace(/au/g, "à");
+  nextText = nextText.replace(/eu/g, "è");
+  nextText = nextText.replace(/iu/g, "ì");
+  nextText = nextText.replace(/oa/g, "ò");
+  nextText = nextText.replace(/ua/g, "ù");
+  return nextText;
+}
+
+window.onload = prepare;
