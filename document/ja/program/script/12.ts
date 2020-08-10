846a351cd4cd62fb553a4fbbf64270148bc7146e
Ziphil <yfe23457@nifty.com>
2020-08-10 12:17:57 +0900

    リファクタリング

--- document/ja/program/script/12.ts
+++ document/ja/program/script/12.ts
@@ -1,36 +1,48 @@
 //
 
 
-const DICTIONARY_URL = "conlang/database/1.cgi";
-
 type DayType = {current: 0, difference: number};
 
-export function prepare(): void {
-  fetch("#whole-count", "current", 0);
-  fetch("#week-count", "difference", 7);
-  fetch("#month-count", "difference", 30);
-}
 
-export function fetch<M extends keyof DayType>(query: string, mode: M, day: DayType[M]): void {
-  let request = new XMLHttpRequest();
-  let url = DICTIONARY_URL + "?mode=fetch";
-  if (mode === "current") {
-    url += "&type=1";
-  } else {
-    url += "&type=3";
-    url += "&agree=" + day;
+export class Executor {
+
+  public prepare(): void {
+    this.fetch("#whole-count", "current", 0);
+    this.fetch("#week-count", "difference", 7);
+    this.fetch("#month-count", "difference", 30);
   }
-  request.open("GET", url, true);
-  request.send(null);
-  request.addEventListener("readystatechange", (event) => {
-    if (request.readyState === 4 && request.status === 200) {
-      let result = request.responseText;
-      let element = document.querySelector(query);
-      if (element !== null) {
-        element.textContent = result;
+
+  private fetch<M extends keyof DayType>(query: string, mode: M, day: DayType[M]): void {
+    let request = new XMLHttpRequest();
+    let url = this.createUrl(mode, day);
+    request.open("GET", url, true);
+    request.send(null);
+    request.addEventListener("readystatechange", (event) => {
+      if (request.readyState === 4 && request.status === 200) {
+        let result = request.responseText;
+        let element = document.querySelector(query);
+        if (element !== null) {
+          element.textContent = result;
+        }
       }
+    });
+  }
+
+  private createUrl<M extends keyof DayType>(mode: M, day: DayType[M]): string {
+    let url = "conlang/database/1.cgi?mode=fetch";
+    if (mode === "current") {
+      url += "&type=1";
+    } else {
+      url += "&type=3";
+      url += "&agree=" + day;
     }
-  });
+    return url;
+  }
+
 }
 
-window.onload = prepare;
+
+let executor = new Executor();
+window.addEventListener("load", () => {
+  executor.prepare();
+});
