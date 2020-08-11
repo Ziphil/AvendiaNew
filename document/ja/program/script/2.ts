7d0b1d88b8b763cd4372505e432ea3b085b5e3b5
Ziphil <yfe23457@nifty.com>
2020-08-12 06:10:59 +0900

    リファクタリング

--- document/ja/program/script/2.ts
+++ document/ja/program/script/2.ts
@@ -1,37 +1,45 @@
 //
 
 
-export function prepare(): void {
-  let element = document.querySelector<HTMLInputElement>("[name=\"search\"]")!;
-  let checkbox = document.querySelector<HTMLInputElement>("#checkbox-conversion-0")!;
-  element.addEventListener("keyup", (event) => {
-    if (checkbox.checked) {
-      let text = element.value;
-      let nextText = convert(text);
-      if (text !== nextText) {
-        element.value = nextText;
+export class Executor {
+
+  public prepare(): void {
+    let element = document.querySelector<HTMLInputElement>("[name=\"search\"]")!;
+    let checkbox = document.querySelector<HTMLInputElement>("#checkbox-conversion-0")!;
+    element.addEventListener("keyup", (event) => {
+      if (checkbox.checked) {
+        let text = element.value;
+        let nextText = this.convert(text);
+        if (text !== nextText) {
+          element.value = nextText;
+        }
       }
-    }
-  });
-  element.focus();
+    });
+    element.focus();
+  }
+
+  private convert(text: string): string {
+    let nextText = text;
+    nextText = nextText.replace(/aa/g, "â");
+    nextText = nextText.replace(/ee/g, "ê");
+    nextText = nextText.replace(/ii/g, "î");
+    nextText = nextText.replace(/oo/g, "ô");
+    nextText = nextText.replace(/uu/g, "û");
+    nextText = nextText.replace(/ai/g, "á");
+    nextText = nextText.replace(/ei/g, "é");
+    nextText = nextText.replace(/ie/g, "í");
+    nextText = nextText.replace(/au/g, "à");
+    nextText = nextText.replace(/eu/g, "è");
+    nextText = nextText.replace(/iu/g, "ì");
+    nextText = nextText.replace(/oa/g, "ò");
+    nextText = nextText.replace(/ua/g, "ù");
+    return nextText;
+  }
+
 }
 
-export function convert(text: string): string {
-  let nextText = text;
-  nextText = nextText.replace(/aa/g, "â");
-  nextText = nextText.replace(/ee/g, "ê");
-  nextText = nextText.replace(/ii/g, "î");
-  nextText = nextText.replace(/oo/g, "ô");
-  nextText = nextText.replace(/uu/g, "û");
-  nextText = nextText.replace(/ai/g, "á");
-  nextText = nextText.replace(/ei/g, "é");
-  nextText = nextText.replace(/ie/g, "í");
-  nextText = nextText.replace(/au/g, "à");
-  nextText = nextText.replace(/eu/g, "è");
-  nextText = nextText.replace(/iu/g, "ì");
-  nextText = nextText.replace(/oa/g, "ò");
-  nextText = nextText.replace(/ua/g, "ù");
-  return nextText;
-}
 
-window.onload = prepare;
+let executor = new Executor();
+window.addEventListener("load", () => {
+  executor.prepare();
+});
