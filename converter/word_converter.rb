413d03fc8c5046a3270d01cc7d09b14cfd31e8c0
Ziphil <yfe23457@nifty.com>
2020-08-12 21:15:22 +0900

    編集名などを変更

--- converter/word_converter.rb
+++ converter/word_converter.rb
@@ -7,11 +7,11 @@ module WordConverter
 
   module_function
 
-  def convert(match, url, link = true, version = 0, left_names = nil, option_string = "")
+  def convert(string, url, link = true, version = 0, left_names = nil, option_string = "")
     left_names ||= DEFAULT_LEFT_NAMES
     if link
-      match = "%" + match.gsub(/\s+/, "% %").gsub("-", "%-%") + "%"
-      match.gsub!(/%([\"\[«…]*)(.*?)([!\?\.,\"\]»…]*)%/) do
+      string = "%" + string.gsub(/\s+/, "% %").gsub("-", "%-%") + "%"
+      string.gsub!(/%([\"\[«…]*)(.*?)([!\?\.,\"\]»…]*)%/) do
         left, matched_name, right = $1, $2, $3
         modified_name = matched_name.gsub(/<\/?\w+>/, "")
         if version == 0 && matched_name.match(/(.+)'(.+)/)
@@ -19,48 +19,48 @@ module WordConverter
           modified_left = abbreviation_left.gsub(/<\/?\w+>/, "")
           modified_right = abbreviation_right.gsub(/<\/?\w+>/, "")
           if left_names.include?("#{modified_left}'")
-            link = left
+            html = left
             if abbreviation_left =~ /^[0-9:]$/
-              link << abbreviation_left + "'"
+              html << abbreviation_left + "'"
             else
-              link << "<a href=\"#{url}?search=#{modified_left}'&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">#{abbreviation_left}'</a>"
+              html << "<a href=\"#{url}?search=#{modified_left}'&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">#{abbreviation_left}'</a>"
             end
             if abbreviation_right =~ /^[0-9:]$/
-              link << abbreviation_right
+              html << abbreviation_right
             else
-              link << "<a href=\"#{url}?search=#{modified_right}&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">#{abbreviation_right}</a>"
+              html << "<a href=\"#{url}?search=#{modified_right}&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">#{abbreviation_right}</a>"
             end
-            link << right
-            next link
+            html << right
+            next html
           else
-            link = left
+            html = left
             if abbreviation_left =~ /^[0-9:]$/
-              link << abbreviation_left
+              html << abbreviation_left
             else
-              link << "<a href=\"#{url}?search=#{modified_left}&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">#{abbreviation_left}</a>"
+              html << "<a href=\"#{url}?search=#{modified_left}&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">#{abbreviation_left}</a>"
             end
             if abbreviation_right =~ /^[0-9:]$/
-              link << "'" + abbreviation_right
+              html << "'" + abbreviation_right
             else
-              link << "<a href=\"#{url}?search='#{modified_right}&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">'#{abbreviation_right}</a>"
+              html << "<a href=\"#{url}?search='#{modified_right}&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">'#{abbreviation_right}</a>"
             end
-            link << right
-            next link
+            html << right
+            next html
           end
         else
-          link = left
+          html = left
           if matched_name =~ /^[0-9:]$|^ʻ|^—$/
-            link << matched_name
+            html << matched_name
           else
-            link << "<a href=\"#{url}?search=#{modified_name}&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">#{matched_name}</a>"
+            html << "<a href=\"#{url}?search=#{modified_name}&amp;mode=search&amp;type=0&amp;agree=0#{option_string}\" rel=\"nofollow\">#{matched_name}</a>"
           end
-          link << right
-          next link
+          html << right
+          next html
         end
       end
-      return "<span class=\"sans\">#{match}</span>"
+      return "<span class=\"sans\">#{string}</span>"
     else
-      return "<span class=\"sans\">#{match}</span>"
+      return "<span class=\"sans\">#{string}</span>"
     end
   end
 
