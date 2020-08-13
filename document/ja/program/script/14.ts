0b9f75a5100368caa823b2ae8dec75089ecf51b2
Ziphil <yfe23457@nifty.com>
2020-08-13 16:55:37 +0900

    造語依頼フォームをCGIを使わない形に変更

---
+++ document/ja/program/script/14.ts
@@ -0,0 +1,43 @@
+//
+
+
+export class Executor {
+
+  public prepare(): void {
+    let textArea = document.querySelector<HTMLTextAreaElement>("[name=\"content\"]")!;
+    document.querySelector("#submit")!.addEventListener("click", (event) => {
+      let request = new XMLHttpRequest();
+      let data = new FormData();
+      data.append("mode", "request");
+      data.append("content", textArea.value);
+      request.open("POST", "../../program/interface/3.cgi", true);
+      request.send(data);
+      request.addEventListener("readystatechange", () => {
+        if (request.readyState === 4 && request.status === 200) {
+          let result = JSON.parse(request.responseText);
+          let size = result.size;
+          this.displayResult(size);
+        }
+      });
+      event.preventDefault();
+    });
+  }
+
+  private displayResult(size: number): void {
+    let element = document.querySelector<HTMLDivElement>("#result")!;
+    let html = "";
+    html += "<h1>依頼完了</h1>\n";
+    html += "<p>\n";
+    html += `造語依頼が完了しました (${size} 件)。\n`;
+    html += "ご協力ありがとうございます。\n";
+    html += "</p>\n";
+    element.innerHTML = html;
+  }
+
+}
+
+
+let executor = new Executor();
+window.addEventListener("load", () => {
+  executor.prepare();
+});
