9f356295b9bd254a07a402b2161c38b10c51c6e8
Ziphil <yfe23457@nifty.com>
2021-01-10 15:53:17 +0900

    ギリシャ語の特殊なダイアクリティカルマークを表示する機能を追加

---
+++ template/greek.rb
@@ -0,0 +1,18 @@
+﻿# coding: utf-8
+
+
+GREEK_DIACTIRICS = {"a" => "´", "g" => "`", "sa" => "῎", "sg" => "῍", "ra" => "῞", "rg" => "῝"}
+
+converter.add(["gt"], ["page"]) do |element|
+  this = ""
+  diacritic_type = element.attribute("d").to_s
+  this << Tag.build("span", "greek") do |this|
+    this << Tag.build("span", "char") do |this|
+      this << apply(element, "page")
+    end
+    this << Tag.build("span", "diacritic") do |this|
+      this << GREEK_DIACTIRICS[diacritic_type]
+    end
+  end
+  next this
+end
