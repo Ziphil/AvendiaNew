ed41a2031ce17bd4ee3dfd7abca4597e3c99c994
Ziphil <yfe23457@nifty.com>
2021-06-19 20:58:47 +0900

    変更

--- document/common/program/script/12.ts
+++ document/common/program/script/12.ts
@@ -8,35 +8,23 @@ import {
 export class Executor extends ExecutorBase {
 
   protected prepare(): void {
-    this.fetch("#whole-count", "current", 0);
-    this.fetch("#week-count", "difference", 7);
-    this.fetch("#month-count", "difference", 30);
-  }
-
-  private fetch(query: string, mode: "current" | "difference", day: number): void {
     let request = new XMLHttpRequest();
-    let url = this.createUrl(mode, day);
+    let url = window.location.protocol + "//dic.ziphil.com/api/dictionary/difference?durations=7&durations=30"
     request.open("GET", url, true);
     request.send(null);
     request.addEventListener("readystatechange", (event) => {
       if (request.readyState === 4 && request.status === 200) {
-        let result = parseInt(request.responseText, 10);
-        let element = document.querySelector(query);
-        if (element !== null) {
-          element.textContent = Math.max(result, 0).toString();
+        let data = JSON.parse(request.responseText);
+        for (let {duration, difference} of data.differences) {
+          if (duration === 7) {
+            document.querySelector("#week-count")!.textContent = Math.max(difference, 0).toString();
+          } else {
+            document.querySelector("#month-count")!.textContent = Math.max(difference, 0).toString();
+          }
         }
+        document.querySelector("#whole-count")!.textContent = Math.max(data.count, 0).toString();
       }
     });
-  }
-
-  private createUrl(mode: "current" | "difference", day: number): string {
-    let url = window.location.protocol + "//dic.ziphil.com/api/dictionary";
-    if (mode === "current") {
-      url += "/count";
-    } else {
-      url += "/difference?duration=" + day;
-    }
-    return url;
   }
 
 }
