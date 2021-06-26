9bfa4e1b867b8da96a74b966e9d11f34e8010295
Ziphil <yfe23457@nifty.com>
2021-06-27 01:40:11 +0900

    トップページに年単位の単語数増減を表示するよう変更

--- document/common/program/script/12.ts
+++ document/common/program/script/12.ts
@@ -9,7 +9,7 @@ export class Executor extends ExecutorBase {
 
   protected prepare(): void {
     let request = new XMLHttpRequest();
-    let url = window.location.protocol + "//dic.ziphil.com/api/dictionary/difference?durations=7&durations=30"
+    let url = window.location.protocol + "//dic.ziphil.com/api/dictionary/difference?durations=7&durations=30&durations=365"
     request.open("GET", url, true);
     request.send(null);
     request.addEventListener("readystatechange", (event) => {
@@ -18,8 +18,10 @@ export class Executor extends ExecutorBase {
         for (let {duration, difference} of data.differences) {
           if (duration === 7) {
             document.querySelector("#week-count")!.textContent = Math.max(difference, 0).toString();
-          } else {
+          } else if (duration === 30) {
             document.querySelector("#month-count")!.textContent = Math.max(difference, 0).toString();
+          } else {
+            document.querySelector("#year-count")!.textContent = Math.max(difference, 0).toString();
           }
         }
         document.querySelector("#whole-count")!.textContent = Math.max(data.count, 0).toString();
