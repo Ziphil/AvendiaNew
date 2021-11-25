ec79a4481d5ef465f063c7e768dea5a1a2559bef
Ziphil <yfe23457@nifty.com>
2021-11-25 22:40:56 +0900

    ギリシャ文字要素で下付きイオタを追加できるよう変更

--- template/greek.rb
+++ template/greek.rb
@@ -1,7 +1,7 @@
 ﻿# coding: utf-8
 
 
-GREEK_DIACTIRICS = {"a" => "´", "g" => "`", "s" => "᾿", "sa" => "῎", "sg" => "῍", "r" => "῾", "ra" => "῞", "rg" => "῝"}
+GREEK_DIACTIRICS = {"a" => "´", "g" => "`", "s" => "᾿", "sa" => "῎", "sg" => "῍", "r" => "῾", "ra" => "῞", "rg" => "῝", "i" => "ι"}
 
 converter.add(["gd"], ["page", "page.section-table"]) do |element|
   this = ""
@@ -10,8 +10,15 @@ converter.add(["gd"], ["page", "page.section-table"]) do |element|
     this << Tag.build("span", "char") do |this|
       this << apply(element, "page")
     end
-    this << Tag.build("span", "diacritic") do |this|
-      this << GREEK_DIACTIRICS[diacritic_type]
+    unless diacritic_type.gsub("i", "") == ""
+      this << Tag.build("span", "diacritic") do |this|
+        this << GREEK_DIACTIRICS[diacritic_type.gsub("i", "")]
+      end
+    end
+    if diacritic_type.include?("i")
+      this << Tag.build("span", "diacritic below") do |this|
+        this << GREEK_DIACTIRICS["i"]
+      end
     end
   end
   next this
